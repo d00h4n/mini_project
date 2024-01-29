c6bdd73c28e11fb04e6de88f7e20d3171042f62a
d00h4n <doohan.mintarso@gmail.com>
2024-01-29 10:04:03 +0700

    mini project

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Halo kawan SSSSSSSSSSSSSSSSSS</h1>
+    <h1>Halo kawan \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\</h1>
 </body>
 </html>
