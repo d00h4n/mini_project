05ac391240674839885938b5a57d9ee1591ac6f1
d00h4n <108811042+d00h4n@users.noreply.github.com>
2024-01-29 15:23:41 +0700

    Update karyawan.sql

--- karyawan.sql
+++ karyawan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 29, 2024 at 07:54 AM
+-- Generation Time: Jan 29, 2024 at 09:22 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `absensi`
+--
+
+CREATE TABLE `absensi` (
+  `id_absensi` tinyint(20) NOT NULL,
+  `kategori` varchar(225) NOT NULL,
+  `gambar` varchar(255) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `approval` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `karyawan`
 --
 
@@ -31,6 +61,8 @@ CREATE TABLE `karyawan` (
   `id` bigint(20) NOT NULL,
   `nama` varchar(225) NOT NULL,
   `id_posisi` int(20) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(255) NOT NULL,
   `tanggal_lahir` date DEFAULT NULL,
   `alamat` varchar(225) DEFAULT NULL,
   `username` varchar(225) DEFAULT NULL,
@@ -46,17 +78,148 @@ CREATE TABLE `karyawan` (
 -- Dumping data for table `karyawan`
 --
 
-INSERT INTO `karyawan` (`id`, `nama`, `id_posisi`, `tanggal_lahir`, `alamat`, `username`, `email`, `password`, `no_hp`, `tanggal_masuk`, `create_at`, `update_at`) VALUES
-(1, 'admin', 1, NULL, 'admin', 'admin', 'admin@gmail.com', '12345678', '08123456789', '2024-01-02', '2024-01-29 06:31:07', '2024-01-29 06:31:07'),
-(2, 'Brian', 2, '2024-01-03', 'salatiga', 'brian1', 'brian1@gmail.com', '1234', '08123456789', '2024-01-10', '2024-01-29 06:33:48', '2024-01-29 06:33:48'),
-(4, 'Briyan', 2, '2024-01-03', 'salatiga', 'Briyan1', 'Briyan1@gmail.com', '1234', '08123456789', '2024-01-02', '2024-01-29 06:45:32', '2024-01-29 06:45:32'),
-(5, 'Briyan2', 2, '2024-01-03', 'salatiga', 'Briyan2', 'Briyan2@gmail.com', '1234', '08123456789', '2024-01-02', '2024-01-29 06:46:17', '2024-01-29 06:46:17'),
-(6, 'Briyan3', 2, '2024-01-03', 'salatiga', 'Briyan3', 'Briyan3@gmail.com', '1234', '08123456789', '2024-01-02', '2024-01-29 06:46:17', '2024-01-29 06:46:17'),
-(7, 'Briyan4', 2, '2024-01-03', 'salatiga', 'Briyan4', 'Briyan4@gmail.com', '1234', '08123456789', '2024-01-02', '2024-01-29 06:46:17', '2024-01-29 06:46:17');
+INSERT INTO `karyawan` (`id`, `nama`, `id_posisi`, `gambar`, `jenis_kelamin`, `tanggal_lahir`, `alamat`, `username`, `email`, `password`, `no_hp`, `tanggal_masuk`, `create_at`, `update_at`) VALUES
+(1, 'admin', 1, '', '', NULL, 'admin', 'admin', 'admin@gmail.com', '12345678', '08123456789', '2024-01-02', '2024-01-29 06:31:07', '2024-01-29 06:31:07'),
+(2, 'Brian', 2, '', '', '2024-01-03', 'salatiga', 'brian1', 'brian1@gmail.com', '1234', '08123456789', '2024-01-10', '2024-01-29 06:33:48', '2024-01-29 06:33:48'),
+(4, 'Briyan', 2, '', '', '2024-01-03', 'salatiga', 'Briyan1', 'Briyan1@gmail.com', '1234', '08123456789', '2024-01-02', '2024-01-29 06:45:32', '2024-01-29 06:45:32'),
+(5, 'Briyan2', 2, '', '', '2024-01-03', 'salatiga', 'Briyan2', 'Briyan2@gmail.com', '1234', '08123456789', '2024-01-02', '2024-01-29 06:46:17', '2024-01-29 06:46:17'),
+(6, 'Briyan3', 2, '', '', '2024-01-03', 'salatiga', 'Briyan3', 'Briyan3@gmail.com', '1234', '08123456789', '2024-01-02', '2024-01-29 06:46:17', '2024-01-29 06:46:17'),
+(7, 'Briyan4', 2, '', '', '2024-01-03', 'salatiga', 'Briyan4', 'Briyan4@gmail.com', '1234', '08123456789', '2024-01-02', '2024-01-29 06:46:17', '2024-01-29 06:46:17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2024_01_29_035902_posisi', 1),
+(6, '2024_01_29_040116_presensi', 1),
+(7, '2024_01_29_040137_absensi', 1),
+(8, '2024_01_29_041005_karyawan', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posisi`
+--
+
+CREATE TABLE `posisi` (
+  `id` int(20) NOT NULL,
+  `n_posisi` varchar(225) NOT NULL,
+  `deskrip` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `posisi`
+--
+
+INSERT INTO `posisi` (`id`, `n_posisi`, `deskrip`, `created_at`, `updated_at`) VALUES
+(1, 'admin', NULL, '2024-01-29 06:22:47', '2024-01-29 06:22:47'),
+(2, 'Game Designer', NULL, '2024-01-29 06:25:07', '2024-01-29 06:25:07'),
+(3, 'Game Developer / Programmer:', NULL, '2024-01-29 06:25:47', '2024-01-29 06:25:47'),
+(4, '3D Artist / Animator', NULL, '2024-01-29 06:27:52', '2024-01-29 06:27:52'),
+(5, '2D Artist / Illustrator', NULL, '2024-01-29 06:27:52', '2024-01-29 06:27:52'),
+(6, 'Game Tester', NULL, '2024-01-29 06:27:52', '2024-01-29 06:27:52'),
+(7, 'Sound Designer / Composer', NULL, '2024-01-29 06:27:52', '2024-01-29 06:27:52'),
+(8, 'Game Producer / Project Manager', NULL, '2024-01-29 06:27:52', '2024-01-29 06:27:52'),
+(9, 'UI/UX Designer', NULL, '2024-01-29 06:27:52', '2024-01-29 06:27:52'),
+(10, 'Game Writer', NULL, '2024-01-29 06:27:52', '2024-01-29 06:27:52'),
+(11, 'Community Manager', NULL, '2024-01-29 06:27:52', '2024-01-29 06:27:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `presensi`
+--
+
+CREATE TABLE `presensi` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam_masuk` time DEFAULT NULL,
+  `jam_pulang` time DEFAULT NULL,
+  `id_absensi` tinyint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD PRIMARY KEY (`id_absensi`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
 -- Indexes for table `karyawan`
@@ -67,14 +230,96 @@ ALTER TABLE `karyawan`
   ADD KEY `id_posisi` (`id_posisi`) USING BTREE;
 
 --
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `posisi`
+--
+ALTER TABLE `posisi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `presensi`
+--
+ALTER TABLE `presensi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_absensi` (`id_absensi`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `absensi`
+--
+ALTER TABLE `absensi`
+  MODIFY `id_absensi` tinyint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posisi`
+--
+ALTER TABLE `posisi`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `presensi`
+--
+ALTER TABLE `presensi`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -85,6 +330,12 @@ ALTER TABLE `karyawan`
 --
 ALTER TABLE `karyawan`
   ADD CONSTRAINT `karyawan_ibfk_1` FOREIGN KEY (`id_posisi`) REFERENCES `posisi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `presensi`
+--
+ALTER TABLE `presensi`
+  ADD CONSTRAINT `presensi_ibfk_1` FOREIGN KEY (`id_absensi`) REFERENCES `absensi` (`id_absensi`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
